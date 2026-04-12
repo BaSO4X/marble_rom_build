de52f446fc60b75de4edf3bc10a74eb201039bd5
BaSO4X <BaSO4X@users.noreply.github.com>
2026-04-12 10:42:01 +0800

    fix systemui

--- make.sh
+++ make.sh
@@ -236,6 +236,7 @@ echo "添加手电筒功能"
 bash "$GITHUB_WORKSPACE"/files/MIUISystemUIPlugin/patch.sh
 cd "$GITHUB_WORKSPACE"/files/MIUISystemUIPlugin
 zip -u "$GITHUB_WORKSPACE"/images/product/app/MIUISystemUIPlugin/MIUISystemUIPlugin.apk classes3.dex
+zipalign -f -p 4 "$GITHUB_WORKSPACE"/images/product/app/MIUISystemUIPlugin/MIUISystemUIPlugin.apk "$GITHUB_WORKSPACE"/images/product/app/MIUISystemUIPlugin/MIUISystemUIPlugin.apk
 End_Time 功能修复
 ### 功能修复结束
 
