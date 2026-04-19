10c516f2ae4230c7a5d47a30386caa83cea04533
BaSO4X <BaSO4X@users.noreply.github.com>
2026-04-19 18:55:56 +0800

    fix camera

--- make.sh
+++ make.sh
@@ -204,6 +204,7 @@ rm -rf "$GITHUB_WORKSPACE"/images/product/priv-app/MiniGameService
 rm -rf "$GITHUB_WORKSPACE"/images/product/priv-app/MIUIBrowser
 rm -rf "$GITHUB_WORKSPACE"/images/product/priv-app/AIService
 rm -rf "$GITHUB_WORKSPACE"/images/product/priv-app/MIService
+rm -rf "$GITHUB_WORKSPACE"/images/product/priv-app/MiuiCamera
 rm -rf "$GITHUB_WORKSPACE"/images/product/pangu/system/app/Nfc_st
 rm -rf "$GITHUB_WORKSPACE"/images/mi_ext/product/ai/taiyi
 echo "精简apk完成"
